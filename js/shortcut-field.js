70f9e42d03014d17f63fa569ba33fd1231892247
Joseph Schmitt <joe@reusablebits.com>
2011-02-28 22:51:57 -0500

    Added instruction text. Exiting edit mode on esc key. Never actually show focus on input field.

--- js/shortcut-field.js
+++ js/shortcut-field.js
@@ -1,19 +1,18 @@
 var ShortcutField = Class.extend({
     init: function(element, options) {
         options = options || {};
-        
+
         var self = this,
             field = document.createElement('input'),
             clearBtn = document.createElement('button'),
             revertBtn = document.createElement('button'),
 
-            savedModifiers,
-            savedKey;
+            setModifiers,
+            setKey;
 
         self.element = element;
 
         field.setAttribute('type', 'text');
-        field.setAttribute('placeholder', 'Click to record shortcut');
 
         addClass(clearBtn, 'clear');
         addClass(revertBtn, 'revert');
@@ -21,56 +20,58 @@ var ShortcutField = Class.extend({
         element.appendChild(field);
         element.appendChild(clearBtn);
         element.appendChild(revertBtn);
+        
+        setInstruction('Click to record shortcut');
 
-        //Events
-        field.addEventListener('focus', enterEditMode);
+
+// PRIVATE METHODS ____________________________________________________________
 
         function enterEditMode() {
+            if (hasClass(element, 'edit')) {
+                return false;
+            }
+            
             //Clear the field
             field.value = '';
-            
+
             addKeyEvents();
             addClass(element, 'edit');
+            
+            setInstruction('Type shortcut');
         }
-
+        
         function exitEditMode() {
+            if (!hasClass(element, 'edit')) {
+                return false;
+            }
+            
             removeKeyEvents();
-            
             removeClass(element, 'edit');
-            
-            if (savedKey) {
+
+            if (setKey) {
                 if (options.update) {
                     options.update.apply(null, []);
                 }
             }
             
-            field.blur();
+            setInstruction('Click to record shortcut');
         }
 
         function addKeyEvents() {
-            field.addEventListener('keydown', onKeyDown)
-            field.addEventListener('keyup', onKeyUp);
-            field.addEventListener('blur', exitEditMode);
+            window.addEventListener('keydown', onKeyDown)
+            window.addEventListener('keyup', onKeyUp);
+            window.addEventListener('click', onClick);
+            // field.addEventListener('blur', exitEditMode);
         }
 
         function removeKeyEvents() {
-            field.removeEventListener('keydown', onKeyDown)
-            field.removeEventListener('keyup', onKeyUp);
-            field.removeEventListener('blur', exitEditMode);
+            window.removeEventListener('keydown', onKeyDown)
+            window.removeEventListener('keyup', onKeyUp);
+            // field.removeEventListener('blur', exitEditMode);
         }
-
-        function onKeyDown(e) {
-            parseKeys(e.keyCode, e);
-        }
-
-        function onKeyUp(e) {
-            // field.blur();
-            if (savedKey) {
-                exitEditMode();
-            }
-            else {
-                parseKeys(e.keyCode, e);
-            }
+        
+        function setInstruction(text) {
+            field.setAttribute('placeholder', text);
         }
         
         function parseKeys(keyCode, e) {
@@ -88,11 +89,11 @@ var ShortcutField = Class.extend({
             if (e.altKey || keyCode == KEY_ALT) {
                 modifiers.push(KEYMAP[KEY_ALT]);
             }
-                
+
             if (e.type == 'keyup') {
                 //If the modifier key was released, remove it
-                if (savedModifiers.indexOf(keyName) > -1) {
-                    modifiers.splice(savedModifiers.indexOf(keyName), 1);
+                if (setModifiers.indexOf(keyName) > -1) {
+                    modifiers.splice(setModifiers.indexOf(keyName), 1);
                 }
             }
 
@@ -101,14 +102,14 @@ var ShortcutField = Class.extend({
             }
 
             updateField(key, modifiers);
-            saveCombo(key, modifiers, e);
-            
+            setCombo(key, modifiers, e);
+
             e.preventDefault();
         }
 
         function updateField(key, modifiers) {
             var text = '';
-            
+
             modifiers.forEach(function(modifier) {
                 text += modifier.name + ' ';
             });
@@ -120,12 +121,60 @@ var ShortcutField = Class.extend({
             field.value = text;
         }
 
-        function saveCombo(key, modifiers) {
-            // console.log('saveCombo', key);
-            savedModifiers = Array.prototype.slice.call(modifiers);
-            savedKey = key;
-            
+        function setCombo(key, modifiers) {
+            setModifiers = Array.prototype.slice.call(modifiers);
+            setKey = key;
+
             if (key) {
+                exitEditMode();
+            }
+        }
+        
+        function saveCombo(key, modifiers) {
+            
+        }
+
+
+// EVENT SUBSCRIPTIONS ________________________________________________________
+
+        //Events
+        field.addEventListener('focus', function(e) {
+            enterEditMode();
+            field.blur();
+        });
+        clearBtn.addEventListener('click', function(e) {
+            enterEditMode();
+        });
+        revertBtn.addEventListener('mouseover', function(e) {
+            setInstruction('Use old shortcut');
+        });
+        revertBtn.addEventListener('mouseout', function(e) {
+            setInstruction('Type shortcut');
+        });
+
+
+// EVENT HANDLERS _____________________________________________________________
+
+        function onKeyDown(e) {
+            if (e.keyCode == KEY_ESC) {
+                return exitEditMode();
+            }
+            
+            parseKeys(e.keyCode, e);
+        }
+
+        function onKeyUp(e) {
+            // field.blur();
+            if (setKey) {
+                exitEditMode();
+            }
+            else {
+                parseKeys(e.keyCode, e);
+            }
+        }
+        
+        function onClick(e) {
+            if (!hasAncestor(e.target, element)) {
                 exitEditMode();
             }
         }
